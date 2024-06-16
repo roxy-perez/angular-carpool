cdd712ae10dcea585cdb2ba9cf8a1f24eedb623c
Roxy <necropsia32@gmail.com>
2024-06-16 20:50:35 +0100

    Added UI Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  isSignDivVisible: boolean = true;
+  signUp: SignUpModel = new SignUpModel();
+  signIn: SignInModel = new SignInModel();
+}
+
+export class SignUpModel {
+  public firstName: string;
+  public lastName: string;
+  public email: string;
+  public password: string;
+  public confirmPassword: string;
+
+  constructor() {
+    this.firstName = '';
+    this.lastName = '';
+    this.email = '';
+    this.password = '';
+    this.confirmPassword = '';
+  }
+}
+
+export class SignInModel {
+  public email: string;
+  public password: string;
+
+  constructor() {
+    this.email = '';
+    this.password = '';
+  }
 }
