a16899d95a24d1718a83c64d030ace0064f7c3e8
Roxy Pérez <necropsia32@gmail.com>
2024-06-16 13:09:55 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: LayoutComponent, children: [
+      { path: '', component: LoginComponent },
+      { path: 'login', component: LoginComponent }
+    ]
+  }
+];
--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-layout',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
 })
-export class AppComponent {
-  title = 'Canary Carpool';
+export class LayoutComponent {
+
 }
