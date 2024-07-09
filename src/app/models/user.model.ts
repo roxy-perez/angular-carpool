0080940f64bf3280f6e874d5b0271f5ecb79f43e
Roxy Pérez <necropsia32@gmail.com>
2024-07-09 16:18:28 +0100

    Add more responsive css

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,9 @@
+export interface User {
+        id: number;
+        name: string;
+        email: string;
+        phone: string;
+        profile_picture: string;
+        created_at: string;
+        updated_at: string;
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'http://localhost:8500/api';
+
+  constructor(private http: HttpClient) { }
+
+  register(data: any): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/user/register`, data);
+  }
+
+  login(data: any): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/user/login`, data);
+  }
+
+  logout(token: string): Observable<any> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post(`${this.apiUrl}/user/logout`, {}, { headers });
+  }
+
+  getUser(token: string): Observable<User> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<User>(`${this.apiUrl}/user`, { headers });
+  }
+}
